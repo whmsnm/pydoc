2534f6fdd09ab4fd14d8c9cd6f106cfac734008d
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 19:06:17 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 sleep 5s;
 
-chmod 777 /app/logs
-
 #get the latest version of the source and add the custom browser.py
-git clone https://github.com/charlesbel/Microsoft-Rewards-Farmer.git ./git > /dev/null 2>&1
+git clone https://github.com/mydatabase000/finalfarshadupdatedwithsearchwordsfour.git ./git > /dev/null 2>&1
 
 chmod 777 /app/src
 
