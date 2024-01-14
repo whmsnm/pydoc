d61d6eb87e301c948d504ae5ca49dbabcdd7c42a
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 13:33:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ sleep 5s;
 
 # install requirements
 export PIP_ROOT_USER_ACTION=ignore
-pip install --upgrade pip > /dev/null
-pip install -r requirements.txt > /dev/null
+pip3 install -r requirements.txt > /dev/null
 
 
 # start the script
