2b659eb64937f05f0fa97b930fe7f581f485da63
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 19:06:36 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -4,5 +4,4 @@ sleep 5s;
 #get the latest version of the source and add the custom browser.py
 git clone https://github.com/mydatabase000/finalfarshadupdatedwithsearchwordsfour.git ./git > /dev/null 2>&1
 
-chmod 777 /app/src
 
