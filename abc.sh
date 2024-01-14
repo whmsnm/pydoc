214201795642d8666ef21b3c0a472ad61c393a97
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 19:02:45 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sleep 5s;
 
+chmod 777 /app/logs
+
 #get the latest version of the source and add the custom browser.py
 git clone https://github.com/charlesbel/Microsoft-Rewards-Farmer.git ./git > /dev/null 2>&1
+
+chmod 777 /app/src
 
