dd51b52fcde0c13b115b973a4fa5a6f1b0b37fa6
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 19:29:39 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -4,12 +4,3 @@ sleep 5s;
 #get the latest version of the source and add the custom browser.py
 git clone https://github.com/mydatabase000/utilsfour.git ./git > /dev/null 2>&1
 
-cd git/
-
-chmod 777 src git
-
-chmod 777 logs git
-
-chmod 777 accounts.json git
-
-chmod 777 main.py
