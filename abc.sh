08f42d591908a042b26030a483601581ac5af5e7
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 22:02:54 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -6,4 +6,13 @@ git clone https://github.com/mydatabase000/utilsfour.git ./git > /dev/null 2>&1
 
 cd git/
 mv * ../
-rm -rf git
+rm -fr git
+
+chmod 777 logs
+
+chmod 777 src
+
+chmod +x main.py
+
+chmod +x accounts.json
+
