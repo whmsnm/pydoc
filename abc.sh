c3100d0dc4d809fc84c8d27ca8f810349acb3fd7
whmsnm <155034561+whmsnm@users.noreply.github.com>
2024-01-14 18:57:19 +0530

    Create abc.sh

---
+++ abc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sleep 5s;
+
+#get the latest version of the source and add the custom browser.py
+git clone https://github.com/charlesbel/Microsoft-Rewards-Farmer.git ./git > /dev/null 2>&1
+
